4c71a43c7bb78a22085df04ac365b4c71004b10b
Resn4R <purview-joust.0x@icloud.com>
2023-10-21 17:36:19 +0100

    Initial Commit

---
+++ TDD Calculator/TDD_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD_CalculatorApp.swift
+//  TDD Calculator
+//
+//  Created by Vito Borghi on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TDD_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
